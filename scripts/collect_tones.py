4ddef795ff37551721d95d4c1e6d915ee8f7ef07
michaelhaaf <michael.haaf@gmail.com>
2022-02-07 23:10:31 -0500

    thai tone unification first draft

---
+++ scripts/collect_tones.py
@@ -0,0 +1,164 @@
+import os
+import re
+import argparse
+import pandas as pd
+
+
+class PronunciationDict:
+    file_keys = ["word", "pronunciation"]
+
+    def __init__(self, tone_dict, col_name):
+        self.tone_dict = tone_dict
+        self.col_name= col_name
+
+    def create_df_from_file(self, file_path):
+        pass
+
+class WikiPronPronunciationDict(PronunciationDict):
+    def __init__(self):
+        tone_dict = {
+                    "˧": "33" ,     # mid
+                    "˨˩": "21" ,    # low
+                    "˩": "21" ,
+                    "˥˩": "43" ,    # falling
+                    "˦˥": "44" ,    # high
+                    "˥": "44" ,
+                    "˩˩˦": "323",   # rising
+                    "˩˦": "323"
+                    }
+        col_name = "wp_tone"
+
+        super().__init__(tone_dict, col_name)
+
+    def create_df_from_file(self, file_path):
+        frame = pd.read_csv(file_path, 
+                delimiter="\t", 
+                header=None, 
+                names=super().file_keys, 
+                index_col="word")
+        return frame
+
+
+class GlobalPhonePronunciationDict(PronunciationDict):
+    def __init__(self):
+        tone_dict = {
+                "1": "1",    
+                "2": "2",
+                "3": "3",
+                "4": "4",
+                "0": "0"
+                }
+        col_name = "gp_tone"
+
+        super().__init__(tone_dict, col_name)
+
+    def create_df_from_file(self, file_path):
+        word_cleanup_pattern = re.compile(r'\(\d+\)')
+        line_break_pattern = re.compile(r'\}\s+')
+        word_pattern = re.compile(r'^{([^{}]+)\s+')
+
+        data_dict = {}
+        with open(file_path, 'r', encoding = 'utf8') as f:
+            for line in f:
+                line = line.strip()
+                if line == '':
+                    continue
+                try:
+                    word, phones = line_break_pattern.split(line, maxsplit=1)
+                except ValueError:
+                    raise(Exception('There was a problem with the line \'{}\'.'.format(line)))
+                if 'SIL' in phones or '+QK' in phones:
+                    continue
+                word = word[1:].strip()
+                if '{' in word:
+                    word = word_pattern.match(line)
+                    word = word.groups()[0]
+                    phones = word_pattern.sub('',line)
+                word = word_cleanup_pattern.sub('', word)
+                word = word.strip()
+                if word not in data_dict:
+                    data_dict[word] = []
+
+                #TODO: not this!!!
+                phonemes = list(phones)
+                phones = " ".join(phonemes)
+
+                data_dict[word].append(phones)
+
+        frame = [ [key, value] for key, value in data_dict.items() ]
+        df = pd.DataFrame(frame, columns=super().file_keys) 
+        df.set_index("word", inplace=True)
+        return df
+
+
+
+
+source_dictionary_map = {
+        "wikiPron": WikiPronPronunciationDict(),
+        "globalPhone": GlobalPhonePronunciationDict()
+        }
+
+
+def tone_lookup(word, df, tone_dict):
+    try:
+        pronunciation = str(df.loc[word].pronunciation)
+    except KeyError:
+        return ["N/A"]
+
+    tones = [tone_dict.get(token) for token in pronunciation.split(" ")] 
+    return list(filter(None, tones))
+
+
+def file_path(path):
+    if os.path.isfile(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"{path} is not a valid file")
+
+
+def main(args):
+
+    df = pd.read_csv(args.intrinsicf0_path, delimiter=",")
+    pronunciation_dict = source_dictionary_map[args.source]
+
+    df[pronunciation_dict.col_name] = df['word'].apply(tone_lookup, 
+            df=pronunciation_dict.create_df_from_file(args.pronunciation_dict_path), 
+            tone_dict=pronunciation_dict.tone_dict)
+
+    print(f"{args.source} tone information computed.")
+    command = input("Preview results, or overwrite {args.intrinsicf0_path}? [p/o]\r\n")
+    if command.lower() != 'o':
+        print("Preview:")
+        print(df)
+    else:
+        print(f"Overwriting {args.intrinsicf0_path}...") 
+        df.to_csv(args.intrinsicf0_path)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description='adds tone information to intrinsicf0 file')
+    parser.add_argument('--pronunciation-dict',
+            required=True,
+            help='path to file containing pronunciation dictionary',
+            type=file_path,
+            dest='pronunciation_dict_path'
+            )
+    parser.add_argument('--intrinsicf0',
+            required=True,
+            help='path to intrinsicf0 file',
+            type=file_path,
+            dest='intrinsicf0_path'
+            )
+    parser.add_argument('--source',
+            required=True,
+            help='wikipron or globalphone?',
+            choices=source_dictionary_map.keys(),
+            dest='source'
+            )
+    args = parser.parse_args()
+
+    main(args)
+
+
