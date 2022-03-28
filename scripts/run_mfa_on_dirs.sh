84b126e13ae7ef29468babf7d10c5dcc21753ebc
michaelhaaf <michael.haaf@gmail.com>
2022-03-28 19:29:04 -0400

    minor fixes to shell script

--- scripts/run_mfa_on_dirs.sh
+++ scripts/run_mfa_on_dirs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e 
 set -u
+set -x
 
 data_dir=$1
 dict=$2
@@ -9,10 +10,10 @@ output_dir=$3
 mapfile -t dirs < <(find "$data_dir" -maxdepth 1 -type d -name "[0-9][0-9]" -printf "%p\n")
 
 for dir in "${dirs[@]}"; do
-    echo "$dir" | awk -v dir="$dir" -v dict="$dict" output="$output_dir$(basename "$dir")" \
+    echo "$dir" | awk -v sub_dir="$dir" -v dict="$dict" -v output="$output_dir/$(basename "$dir")" \
     '{
-        printf "echo Running mfa train on dir %s...;", dir
-        printf "mfa train %s %s %s;", dir, dict, output
+        printf "echo Running mfa train on dir %s...;", sub_dir
+        printf "mfa train %s %s %s;", sub_dir, dict, output
         printf "sleep 2;"
     }' | bash
 done
